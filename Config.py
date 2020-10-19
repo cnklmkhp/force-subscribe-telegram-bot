8ca5d4d082dd61ddfce2652a3b2bb3cc31102ab7
cnklmkhp <73093775+cnklmkhp@users.noreply.github.com>
2020-10-19 12:16:51 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,10 +11,10 @@ class Config():
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1326462146:AAHwdes0JfLsuHUSfCbRSj_EYBFYKu7Dxyg"
+    DATABASE_URL = "postgres://bjldglvdaxtteh:188b494fc341df4c7ab75161f28fd0da3ebea8f7775ab85d7cebd1565790256b@ec2-54-81-37-115.compute-1.amazonaws.com:5432/d92i1o9pmotdg0"
+    APP_ID = "1990511"
+    API_HASH = "a4c5ee4c231eaf12c54a891381819acf"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
@@ -24,13 +24,7 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
-        
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
-        
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
-        
-        "**Developed by @viperadnan**"
+        "**rusak**"
       ]
 
-      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__rusak"
